c9bab582f4ccf5b7925a6d6a47ef4f554bbd5e8b
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-03-04 17:10:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,42 @@
 
 
 double pown(double value, uint16_t n) {
-
+  if (n == 1)
+    return value;
+  else if (n == 0)
+    return 1;
+  else
+    return value * pown(value, n - 1);
 }
 
 uint64_t fact(uint16_t n) {
-
+  if ((n == 0) || (n == 1))
+    return 1;
+  else
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return pown(x, n) * 1. / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+  if (count == 0)
+    return calcItem(x, count);
+  else
+    return calcItem(x, count) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
-
+  if (count == 0)
+    return 0;
+  else
+    return (pown(-1, count - 1) * calcItem(x, 2 * count - 1)) + sinn(x, count - 1);
 }
 
 double cosn(double x, uint16_t count) {
-
+  if (count == 0)
+    return 0;
+  else
+    return pown(-1, count - 1) * calcItem(x, 2 * (count - 1)) + cosn(x, count - 1);
 }
